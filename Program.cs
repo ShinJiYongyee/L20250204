fab014298c80c708d88ef232d2e1bcbc3702d006
shinjiyong <shinjiyong0904@gmail.com>
2025-02-04 12:52:52 +0900

    블랙잭

--- Program.cs
+++ Program.cs
@@ -4,29 +4,30 @@ namespace L20250204
 {
     internal class Program
     {
-        static string[] MakeDeck(string[] deck)
+        //블랙잭
+        //ai와 플레이어가 카드 3장씩을 뽑아
+        //점수가 높은 쪽이 승리
+
+        static int Score(int a)
         {
-            int[] index = new int[52];
-            Random random = new Random();
-
-            for (int i = 0; i < index.Length; i++)
+            //A = 11
+            //2~10 = 2~10
+            //J, K, Q = 10
+            switch (a % 13)
             {
-                index[i] = i + 1;
+                case 1:
+                    return 11;  //A
+                case 11:
+                    return 10;  //J
+                case 12:
+                    return 10;  //Q
+                case 0:
+                    return 10;  //K
+                default:
+                    return a%13;   //2~9
             }
-
-            for (int i = 0; i < index.Length; i++)
-            {
-                int randomNumber = random.Next(i, 52);
-                (index[i], index[randomNumber]) = (index[randomNumber], index[i]);
-            }
-
-            for (int i = 0; i < deck.Length; i++)
-            {
-
-                deck[i] = TrumpSuit(index[i]) + " " + Numbering(index[i]);
-            }
-            return deck;
         }
+
         static string Numbering(int a)
         {
             if (a%13 == 1)
@@ -74,14 +75,53 @@ namespace L20250204
                 return " ";
             }
         }
-        static void Print(string[] deck, int number)
+        static void Print(int[] deckScore, int[] deckName)
         {
+            int computerScore=deckScore[0]+deckScore[1]+deckScore[2];
+            int playerScore = deckScore[3] + deckScore[4]+deckScore[5];
 
-            for(int i = 0;i < number; i++)
+
+            for (int i = 0; i < 6; i++)
             {
-                Console.WriteLine(deck[i]);
+                Console.WriteLine(TrumpSuit(deckName[i]) + " " + Numbering(deckName[i]) + " "+ deckScore[i]);
+
             }
+
+            Console.WriteLine($"computerScore: {computerScore}");
+            Console.WriteLine($"playerScore: {playerScore}");
+            if (computerScore > playerScore)
+            {
+                Console.WriteLine("computer win!");
+            }
+            else 
+            {
+                Console.WriteLine("player win!");
+            }
+
+
         }
+        static int[] MakeDeck(int[] deckScore, int[] deckName)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < deckName.Length; i++)
+            {
+                deckName[i] = i + 1;
+            }
+
+            for (int i = 0; i < deckName.Length; i++)
+            {
+                int randomNumber = random.Next(i, 52);
+                (deckName[i], deckName[randomNumber]) = (deckName[randomNumber], deckName[i]);
+            }
+
+            for (int i = 0; i < deckScore.Length; i++)
+            {
+                deckScore[i] = Score(deckName[i]);
+            }
+            return deckName;
+        }
+
         static void Main(string[] args)
         {
 
@@ -90,11 +130,12 @@ namespace L20250204
             //27 -> 39 : clover
             //40 -> 52 : spade
 
-            string[] deck = new string[52];
+            int[] deckScore = new int[52];
+            int[] deckName = new int[52];
 
-            MakeDeck(deck);
+            MakeDeck(deckScore, deckName);
 
-            Print(deck, 8);
+            Print(deckScore, deckName);
 
         }
     }
