0f98cc63f1d83264bd5b1a7f9387a7b58aadf2c5
shinjiyong <shinjiyong0904@gmail.com>
2025-02-04 11:47:32 +0900

    트럼프 카드 8장 뽑기

--- Program.cs
+++ Program.cs
@@ -1,25 +1,88 @@
-﻿namespace L20250204
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace L20250204
 {
     internal class Program
     {
+        static string Numbering(int a)
+        {
+            if (a%13 == 1)
+            {
+                return "A";
+            }
+            else if (a%13 == 11)
+            {
+                return "J";
+            }
+            else if(a%13 == 12)
+            {
+                return "Q";
+            }
+            else if(a %13 == 0)
+            {
+                return "K";
+            }
+            else
+            {
+                return $"{a%13}";
+            }
+        }
+
+        static string TrumpSuit(int a) 
+        {
+            if (a < 14&&a>0)
+            {
+                return "heart";
+            }
+            else if( a < 27&&a>13 )
+            {
+                return "diamond";
+            }
+            else if( a < 40&&a>26)
+            {
+                return "clover";
+            }
+            else if(a < 53 && a > 39)
+            {
+                return "spade";
+            }
+            else
+            {
+                return " ";
+            }
+        }
         static void Main(string[] args)
         {
 
-            Random random = new();
+            //1 -> 13 : heart (1 : A / 11 : J, 12 : Q, 13 : K)
+            //14 -> 26 : diamond
+            //27 -> 39 : clover
+            //40 -> 52 : spade
 
-            //확률 10퍼센트 가챠 메커니즘
-            for (int i = 0; i < 10; i++) 
+            int[] index = new int[52];
+            Random random = new Random();
+            string[] deck = new string[52];
+
+            for(int i = 0; i < index.Length; i++)
             {
-                int gacha=random.Next(0,100);
+                index[i] = i+1;
+            }
 
-                if (gacha <= 9)
-                {
-                    Console.WriteLine("픽업 성공");
-                }
-                else 
-                {
-                    Console.WriteLine("꽝");
-                }
+            for (int i = 0; i < index.Length; i++)
+            {
+                int randomNumber = random.Next(0, 51);
+                (index[i], index[randomNumber])=(index[randomNumber],index[i]);
+            }
+
+            for (int i = 0; i < deck.Length; i++)
+            {
+
+                deck[i] = TrumpSuit(index[i]) +" "+ Numbering(index[i]);
+            }
+
+            for(int i = 0;i < 8; i++)
+            {
+                Console.WriteLine(deck[i]);
             }
 
         }
