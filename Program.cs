22b4d703f10f977f4a0982b3db1c1eb9d9101274
shinjiyong <shinjiyong0904@gmail.com>
2025-02-04 09:20:19 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace L20250204
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array=new int[52];
+            for(int i=0; i<array.Length; i++)
+            {
+                array[i] = i+1;
+            }
+
+            Random random = new Random();
+
+            //Fisher-Yates shuffle
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomIndex=random.Next(array.Length);
+                int temp=array[randomIndex];
+                array[randomIndex] = array[i];
+                array[i]=temp;
+            }
+
+            //print
+            for (int i = 0;i < 8; i++)
+            {
+                Console.WriteLine($"{array[i]}");
+            }
+        }
+    }
+}
